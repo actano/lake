2f8b0af112b5520a8fc0a57e533f26ffeb45ef91
Anton Wilhelm <timaschew@gmail.com>
2013-10-15 17:59:33 +0200

    fix path in compile script

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@
 rm -rf lib
 
 # compile from src to lib
-./node_modules/.bin/coffee -o lib -c src
+`npm bin`/coffee -o lib -c src
 
 # copy make-feature assets (ignore coffee files, which already coverted to js)
 rsync -av --exclude "*.coffee" src/make-feature lib/make-feature
